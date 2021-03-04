fbb17960ef2c57c6ce4a87b8ab3f39eb42708cd9
Quinn Sweeney <quinn.sweeney@cakeinsure.com>
2021-03-04 16:16:07 -0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const strings = require('./strings');
+
+document.getElementById('about_p1').innerHTML = strings.about.p1;
+
+console.log(aboutStrings);
---
+++ src/strings.js
@@ -0,0 +1,10 @@
+const strings = {
+  about: {
+    p1:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque consectetur metus a mollis fringilla. Integer sem velit, sagittis a auctor ac, pretium vel sem. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur ornare maximus odio ullamcorper pretium. Pellentesque quis volutpat dolor. Nam sed dignissim dolor. In aliquam vitae turpis sed faucibus. Morbi risus nisi, molestie vitae lectus ut, commodo scelerisque lectus. In hac habitasse platea dictumst. Vestibulum id libero a augue pharetra aliquam eu a ipsum. Fusce finibus pellentesque turpis, in imperdiet nulla venenatis id. Praesent imperdiet tortor a mi scelerisque pellentesque. Proin varius fringilla commodo. Nullam hendrerit feugiat leo, at elementum enim finibus quis. ',
+    p2:
+      'Aenean eget lacus in massa pellentesque consectetur et vitae tortor. Cras vulputate tellus est, vel euismod odio pharetra ac. Mauris urna lorem, finibus non accumsan in, efficitur vel justo. Vestibulum nunc elit, finibus non dui eget, pulvinar pellentesque diam. Sed consectetur aliquet viverra. Pellentesque iaculis nunc metus, at feugiat quam sagittis ut. Cras a malesuada dui, eget auctor mauris. Nam ac massa in nulla eleifend condimentum. Vivamus maximus interdum libero sed molestie. Etiam nunc mi, maximus non vulputate id, viverra nec sem. Curabitur bibendum ultrices tempor. Donec lobortis nisi tellus. Duis facilisis venenatis ipsum, ac imperdiet libero. ',
+  },
+};
+
+export default strings;
