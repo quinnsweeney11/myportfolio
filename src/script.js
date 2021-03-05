de4d1ad69c25ae4d3d18a891928e8ba38d2182f9
Quinn Sweeney <quinn.sweeney@cakeinsure.com>
2021-03-05 16:59:10 -0700

    added contact page and also added some styling

--- src/script.js
+++ src/script.js
@@ -1,4 +1,18 @@
 import { strings } from './strings.js';
 
-document.getElementById('about_p1').innerHTML = strings.about.p1;
-document.getElementById('about_p2').innerHTML = strings.about.p2;
+if (document.getElementById('index')) {
+  document.getElementById('about_p1').innerHTML = strings.about.p1;
+  document.getElementById('about_p2').innerHTML = strings.about.p2;
+}
+
+if (document.getElementById('contact')) {
+  let githubIcon = document.createElement('img');
+  githubIcon.src = strings.media.icons.github;
+  document.getElementById('github').appendChild(githubIcon);
+  let gmailIcon = document.createElement('img');
+  gmailIcon.src = strings.media.icons.gmail;
+  document.getElementById('gmail').appendChild(gmailIcon);
+  let twitterIcon = document.createElement('img');
+  twitterIcon.src = strings.media.icons.twitter;
+  document.getElementById('twitter').appendChild(twitterIcon);
+}
